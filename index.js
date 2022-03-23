0e38982857f138a8a4c46f495ca9a16c7f6b4830
Pepe <webpedrod@gmail.com>
2022-03-22 22:17:28 -0300

    Arquivos finais

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const custonExpress = require('./config/customExpress')
 
 const conexao = require('./infraestrutura/conexao')
 
+const Tabelas = require('./infraestrutura/tabelas')
+
 conexao.connect(erro => {
     if(erro) {
         console.log(erro)
     } else {
         console.log("Conectado com sucesso.")
+
+        Tabelas.init (conexao)
         const app = custonExpress()
         app.listen(3000, () => console.log('servidor rodando na porta 3000'))
     }
